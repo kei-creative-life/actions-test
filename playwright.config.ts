111af3decdaa0e87f8b966b57da13b227847e5ec
kei_creativelife <kenken19920616@gmail.com>
2024-07-01 13:27:59 +0900

    パスを修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,11 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   snapshotDir: "tests/__snapshot__",
+  outputDir: "./tests/playwright-report",
+  reporter: [
+    ["html", { outputFolder: "./tests/playwright-report" }],
+    ["json", { outputFile: "./tests/playwright-report/test-results.json" }],
+  ],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,7 +27,6 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
