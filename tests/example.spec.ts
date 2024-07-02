90355a7aa09417f4420e91f2d9fcbbe9dd56af5c
kei_creativelife <kenken19920616@gmail.com>
2024-07-02 12:43:54 +0900

    actionの処理を修正

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,12 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("TOPページのスクリーンショット", async ({ page }, testInfo) => {
+test("TOPページのスクリーンショット", async ({ page }) => {
   // console.log(testInfo);
-  const { project, title, titlePath } = testInfo;
+  // const { project, title, titlePath } = testInfo;
   await page.goto("http://localhost:3000");
-  await page.screenshot({
-    fullPage: true,
-    path: `tests/__snapshot__/${titlePath[0]}_${title}_${project.name}.png`, // TODO: 変数にしたい
-  });
+  // await page.screenshot({
+  //   fullPage: true,
+  //   path: `tests/__snapshot__/${titlePath[0]}_${title}_${project.name}.png`, // TODO: 変数にしたい
+  // });
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
