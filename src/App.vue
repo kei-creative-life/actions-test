9b746fd369e3e70bcae7c2b285724d4ebabdb7e9
kei_creativelife <kenken19920616@gmail.com>
2024-07-09 00:21:37 +0900

    vue router追加

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <script setup></script>
 
 <template>
-  <p>
-    Donate: If you use this site regularly and would like to help keep the site
-    on the Internet, please consider donating a small sum to help pay for the
-    hosting and bandwidth bill. There is no minimum donation, any sum is
-    appreciated - click here to donate using PayPal. Thank you for your support.
-    Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDjb4tyF
-  </p>
-  <h4>
-    It is a long established fact that a reader will be distracted by the
-    readable content of a page when looking at its layout. The point of using
-    Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
-    opposed to using 'Content here, content here', making it look like readable
-    English. Many desktop publishing packages and web page editors now use Lorem
-    Ipsum as their default model text, and a search for 'lorem ipsum' will
-    uncover many web sites still in their infancy. Various versions have evolved
-    over the years, sometimes by accident, sometimes on purpose (injected humour
-    and the like).
-  </h4>
-  <!-- <h4>
-    It is a long established fact that a reader will be distracted by the
-    readable content of a page when looking at its layout. The point of using
-    Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
-    opposed to using 'Content here, content here', making it look like readable
-    English. Many desktop publishing packages and web page editors now use Lorem
-    Ipsum as their default model text, and a search for 'lorem ipsum' will
-    uncover many web sites still in their infancy. Various versions have evolved
-    over the years, sometimes by accident, sometimes on purpose (injected humour
-    and the like).
-  </h4> -->
+  <h1>Hello App!</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,13 +2,9 @@ import { test } from "@playwright/test";
 
 test("TOPページのスクリーンショット", async ({ page }) => {
   await page.goto("http://localhost:3000");
-  // await page.screenshot({
-  //   fullPage: true,
-  //   path: `tests/__snapshot__/${titlePath[0]}_${title}_${project.name}.png`,
-  // });
 
   await page.screenshot({
-    path: "./tests/directory_contains_actual_images/example.png",
+    path: "./tests/directory_contains_actual_images/top.png",
     fullPage: true,
   });
 });
