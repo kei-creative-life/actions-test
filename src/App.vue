e239c04dcaf507a5d6d6982089d4c1bfd6be7c78
kei_creativelife <kenken19920616@gmail.com>
2024-07-08 17:55:16 +0900

    修正

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup></script>
 
 <template>
-  <h1>変更しました</h1>
-  <h2>変更しました!</h2>
-  <h3>変更しました!!!</h3>
-  <h4>変更しました!!!</h4>
+  <h4>
+    It is a long established fact that a reader will be distracted by the
+    readable content of a page when looking at its layout. The point of using
+    Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
+    opposed to using 'Content here, content here', making it look like readable
+    English. Many desktop publishing packages and web page editors now use Lorem
+    Ipsum as their default model text, and a search for 'lorem ipsum' will
+    uncover many web sites still in their infancy. Various versions have evolved
+    over the years, sometimes by accident, sometimes on purpose (injected humour
+    and the like).
+  </h4>
 </template>
 
 <style scoped>
