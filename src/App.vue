0a209d6e135ebe53a13e5cb0b8a727b32e17e5da
kei_creativelife <kenken19920616@gmail.com>
2024-07-08 18:34:27 +0900

    差分追加

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,6 @@
     appreciated - click here to donate using PayPal. Thank you for your support.
     Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDjb4tyF
   </p>
-  <h4>
-    It is a long established fact that a reader will be distracted by the
-    readable content of a page when looking at its layout. The point of using
-    Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
-    opposed to using 'Content here, content here', making it look like readable
-    English. Many desktop publishing packages and web page editors now use Lorem
-    Ipsum as their default model text, and a search for 'lorem ipsum' will
-    uncover many web sites still in their infancy. Various versions have evolved
-    over the years, sometimes by accident, sometimes on purpose (injected humour
-    and the like).
-  </h4>
 </template>
 
 <style scoped>
